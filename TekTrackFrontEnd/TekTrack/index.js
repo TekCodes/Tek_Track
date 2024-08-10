c3c4dd731645f22e74c71f8ec608f97117ac3d62
Alice Gonzalez <144973566+AliceGonzalez@users.noreply.github.com>
2024-08-10 12:13:21 -0400

    Working on App.js

--- TekTrackFrontEnd/TekTrack/index.js
+++ TekTrackFrontEnd/TekTrack/index.js
@@ -1,8 +1,30 @@
-import { registerRootComponent } from 'expo';
 
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+// import './assets/styles/index.css'
+
+// //Creates a root and render the App component into the DOM
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//     <React.StrictMode>
+//         <App />
+//     </React.StrictMode>
+// )
+
+//ORIGINAL CODE
+// import { registerRootComponent } from 'expo';
+
+// import App from './App';
+
+// // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// // It also ensures that whether you load the app in Expo Go or in a native build,
+// // the environment is set up appropriately
+// registerRootComponent(App);
+
+// index.js
+import { AppRegistry } from 'react-native';
 import App from './App';
+import { name as appName } from './app.json';
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
-registerRootComponent(App);
+AppRegistry.registerComponent(appName, () => App);
