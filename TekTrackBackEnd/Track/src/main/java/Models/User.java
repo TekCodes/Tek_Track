250a0df87ebcc06f2cf45b56b4804d8ed14e8a00
matthew123003 <matthew123003@gmail.com>
2024-08-12 14:12:31 -0400

    Started with basic user model class setup

--- TekTrackBackEnd/Track/src/main/java/Models/User.java
+++ TekTrackBackEnd/Track/src/main/java/Models/User.java
@@ -13,4 +13,24 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long userId;
 
+    private String fName;
+
+    private String lName;
+
+    private String userName;
+
+    private String password;
+
+    public User(){}
+
+    public User (long userId, String fName, String lName, String userName, String password){
+        this.userId = userId;
+        this.fName = fName;
+        this.lName = lName;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    
+
 }
