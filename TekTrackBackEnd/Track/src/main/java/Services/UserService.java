a914150f3f49d9f1d34d825708a35f6cd535a7cf
matthew123003 <matthew123003@gmail.com>
2024-08-13 18:37:53 -0400

    Started UserService

--- TekTrackBackEnd/Track/src/main/java/Services/UserService.java
+++ TekTrackBackEnd/Track/src/main/java/Services/UserService.java
@@ -1,8 +1,20 @@
 package Services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import Repositories.UserRepository;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    
 
 }
