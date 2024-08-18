b1b2ead68c18ba766c099abdd8bf8ebcd0b8f6b3
matthew123003 <matthew123003@gmail.com>
2024-08-18 13:17:58 -0400

    Added method to get all users

--- TekTrackBackEnd/Track/src/main/java/Controllers/UserController.java
+++ TekTrackBackEnd/Track/src/main/java/Controllers/UserController.java
@@ -1,9 +1,12 @@
 package Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import Models.User;
 import Services.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,6 +21,11 @@ public class UserController {
 
     public UserController(UserService userService){
         this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<Iterable<User>> getAllUsers(){
+        return new ResponseEntity<>(userService.findAll(), HttpStatus.OK);
     }
 
     
