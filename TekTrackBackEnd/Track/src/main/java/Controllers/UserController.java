5009c42301cc5cca3c9f44a74b2b7693db81aea3
matthew123003 <matthew123003@gmail.com>
2024-08-18 13:14:17 -0400

    Started User Controller

--- TekTrackBackEnd/Track/src/main/java/Controllers/UserController.java
+++ TekTrackBackEnd/Track/src/main/java/Controllers/UserController.java
@@ -1,8 +1,26 @@
 package Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import Services.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 @RestController
+@RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    
+    
 
 }
