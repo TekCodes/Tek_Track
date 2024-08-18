e8ec3ba98dbca1b712bfd6def70becb923d74d8e
matthew123003 <matthew123003@gmail.com>
2024-08-18 13:20:20 -0400

    Added getUserById method

--- TekTrackBackEnd/Track/src/main/java/Controllers/UserController.java
+++ TekTrackBackEnd/Track/src/main/java/Controllers/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import Models.User;
 import Services.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -26,6 +27,11 @@ public class UserController {
     @GetMapping("/users")
     public ResponseEntity<Iterable<User>> getAllUsers(){
         return new ResponseEntity<>(userService.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        return new ResponseEntity<>(userService.findById(id), HttpStatus.OK);
     }
 
     
