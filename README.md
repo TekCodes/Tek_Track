75d38204b0e4e6b7851c0d75a4c07332a869ea59
Alice Gonzalez <144973566+AliceGonzalez@users.noreply.github.com>
2024-08-19 12:32:13 -0400

    Working on interview class

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # TekTrack
-Welcome to TechTrack!
+Welcome to TekTrack!
 Your Ultimate Companion for Managing Tech Opportunities
 
 At TechTrack, we understand that navigating the tech industry can be both exciting and overwhelming. That’s why we’ve created a comprehensive solution to help you streamline your tech career journey. Whether you’re applying for internships, tracking job applications, preparing for interviews, or managing offers, TechTrack brings everything you need into one intuitive platform.
--- TekTrackBackEnd/Track/src/main/java/Models/Interview.java
+++ TekTrackBackEnd/Track/src/main/java/Models/Interview.java
@@ -1,5 +1,62 @@
 package Models;
 
+import java.sql.Date;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name= "Interview")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 public class Interview {
+
+    //Unique table ID
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long interviewId;
+
+    //Table columns
+    @Column(nullable = false, length = 100)
+    private Date interviewDate;
+  
+    @Column(nullable = false, length = 50)
+    private String stage; //Possible ENUM (1st, 2nd, Final)
+    
+    @ Column(name = "ty_note")
+    private Boolean tyNote;
+
+    @Column(name = "interview_type")
+    private String interviewType; //Possible ENUM (In Person, Virtual, Zoom, GoogleMeets, Phone)
+
+    @Column(name = "interview_link", length = 154)
+    private String interviewLink;
+
+    @Column(name = "interview_status")
+    private String interviewStatus;
+
+    @Column(name = "interview_contact_name")
+    private String interviewContactName;
+
+    @Column(name = "interview_contact_email")
+    private String interviewContactEmail;
+
+
+    //Job Info instance //Many to many
+    //Internship Instance //Many to many
+   
+    //Constructors
+    public Interview() {}
+
+    public Interview(Date interviewDate, String stage, Boolean tyNote, String interviewType, String interviewLink) {}
+
+
+
+
+
+
 
 }
