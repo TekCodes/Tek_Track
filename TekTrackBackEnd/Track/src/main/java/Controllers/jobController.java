1cf0e8db2e467b3652cef80c07c90104e6d22798
matthew123003 <matthew123003@gmail.com>
2024-08-19 12:59:11 -0400

    Added PostMapping method

--- TekTrackBackEnd/Track/src/main/java/Controllers/jobController.java
+++ TekTrackBackEnd/Track/src/main/java/Controllers/jobController.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,7 +33,14 @@ public class JobController {
 
     @GetMapping("/job")
     public ResponseEntity<JobInfo> getJob(@PathVariable Long id) {
-        return new ResponseEntity<>(jobService.findById(id), HttpStatus.OK)
+        return new ResponseEntity<>(jobService.findById(id), HttpStatus.OK);
     }
+
+    @PostMapping("/new_job")
+    public ResponseEntity<JobInfo> create(@RequestBody JobInfo jobInfo) {
+        return new ResponseEntity<>(jobService.create(jobInfo), HttpStatus.CREATED);
+    }
+
+    
 
 }
