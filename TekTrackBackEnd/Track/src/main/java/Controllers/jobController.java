36134c0ebaaec72d0a32179357af473196fc82e9
matthew123003 <matthew123003@gmail.com>
2024-08-19 12:30:43 -0400

    Started Job Controller

--- TekTrackBackEnd/Track/src/main/java/Controllers/jobController.java
+++ TekTrackBackEnd/Track/src/main/java/Controllers/jobController.java
@@ -2,9 +2,11 @@ package Controllers;
 
 import Services.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/jobs")
 public class JobController {
     @Autowired
     private JobService jobService;
@@ -12,5 +14,7 @@ public class JobController {
     public JobController(JobService jobService) {
         this.jobService = jobService;
     }
+
+    
 
 }
