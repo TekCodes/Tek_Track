d8a428acba80503df0c6ba5c0a4476b8c8259807
matthew123003 <matthew123003@gmail.com>
2024-08-18 21:25:44 -0400

    JobInfo model initally done, Started JobService

--- TekTrackBackEnd/Track/src/main/java/Services/jobService.java
+++ TekTrackBackEnd/Track/src/main/java/Services/jobService.java
@@ -1,8 +1,15 @@
 package Services;
 
 import Repositories.JobRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import Models.JobInfo;
+
 @Service
 public class JobService {
 
@@ -13,5 +20,12 @@ public class JobService {
         this.jobRepository = jobRepository;
     }
 
+    public List<JobInfo> findAll() { // Method adjusted to return a list of all users
+        Iterable<JobInfo> jobInfoIterable = jobRepository.findAll();
+        List<JobInfo> jobInfoList = new ArrayList<>();
+        jobInfoIterable.forEach(jobInfoList::add);
+        return jobInfoList; 
+    }
 
+    
 }
