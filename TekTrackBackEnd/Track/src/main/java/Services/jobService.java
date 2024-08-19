e5f5c3a8482bf8fbe7579fc1426f94e2eebb1937
matthew123003 <matthew123003@gmail.com>
2024-08-18 20:47:30 -0400

    Started JobService

--- TekTrackBackEnd/Track/src/main/java/Services/jobService.java
+++ TekTrackBackEnd/Track/src/main/java/Services/jobService.java
@@ -1,7 +1,17 @@
 package Services;
 
+import Repositories.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 @Service
 public class JobService {
 
+    @Autowired
+    private JobRepository jobRepository;
+
+    public JobService(JobRepository jobRepository) {
+        this.jobRepository = jobRepository;
+    }
+
+    
 }
