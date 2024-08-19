95234c63a071a4a25f833d9d596755f2464125e8
matthew123003 <matthew123003@gmail.com>
2024-08-18 20:44:03 -0400

    Rearranged fields and added hashcode method

--- TekTrackBackEnd/Track/src/main/java/Models/jobInfo.java
+++ TekTrackBackEnd/Track/src/main/java/Models/jobInfo.java
@@ -1,6 +1,7 @@
 package Models;
 
 import java.util.Date;
+import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 
@@ -19,6 +20,8 @@ public class JobInfo {
 
     private String jobTitle;
 
+    private String jobUrlLink;
+
     private Date dateApplied;
     
     private String contactName;
@@ -33,126 +36,16 @@ public class JobInfo {
 
     private Boolean gotResponse;
 
-    private String jobUrlLink;
+
 
 
     public JobInfo() {
     }
 
-    public JobInfo(String company, String jobTitle, Date dateApplied, String contactName, String contactEmail, String contactNumber, String referral, Boolean remote, Boolean gotResponse, String jobUrlLink) {
-        this.company = company;
-        this.jobTitle = jobTitle;
-        this.dateApplied = dateApplied;
-        this.contactName = contactName;
-        this.contactEmail = contactEmail;
-        this.contactNumber = contactNumber;
-        this.referral = referral;
-        this.remote = remote;
-        this.gotResponse = gotResponse;
-        this.jobUrlLink = jobUrlLink;
-    }
 
-    public JobInfo(long jobInfoId, String company, String jobTitle, Date dateApplied, String contactName, String contactEmail, String contactNumber, String referral, Boolean remote, Boolean gotResponse, String jobUrlLink) {
-        this.jobInfoId = jobInfoId;
-        this.company = company;
-        this.jobTitle = jobTitle;
-        this.dateApplied = dateApplied;
-        this.contactName = contactName;
-        this.contactEmail = contactEmail;
-        this.contactNumber = contactNumber;
-        this.referral = referral;
-        this.remote = remote;
-        this.gotResponse = gotResponse;
-        this.jobUrlLink = jobUrlLink;
-    }
 
-    public long getJobInfoId() {
-        return jobInfoId;
+    @Override
+    public int hashCode() { // Overridden method to generate a hash code for the User object.
+        return Objects.hash(jobInfoId, company, jobTitle, jobUrlLink, dateApplied, contactName, contactEmail, contactNumber, referral, remote, gotResponse); // Returns a hash code
     }
-
-    public void setJobInfoId(long jobInfoId) {
-        this.jobInfoId = jobInfoId;
-    }
-
-    public String getCompany() {
-        return company;
-    }
-
-    public void setCompany(String company) {
-        this.company = company;
-    }
-
-    public String getJobTitle() {
-        return jobTitle;
-    }
-
-    public void setJobTitle(String jobTitle) {
-        this.jobTitle = jobTitle;
-    }
-
-    public Date getDateApplied() {
-        return dateApplied;
-    }
-
-    public void setDateApplied(Date dateApplied) {
-        this.dateApplied = dateApplied;
-    }
-
-    public String getContactName() {
-        return contactName;
-    }
-
-    public void setContactName(String contactName) {
-        this.contactName = contactName;
-    }
-
-    public String getContactEmail() {
-        return contactEmail;
-    }
-
-    public void setContactEmail(String contactEmail) {
-        this.contactEmail = contactEmail;
-    }
-
-    public String getContactNumber() {
-        return contactNumber;
-    }
-
-    public void setContactNumber(String contactNumber) {
-        this.contactNumber = contactNumber;
-    }
-
-    public String getReferral() {
-        return referral;
-    }
-
-    public void setReferral(String referral) {
-        this.referral = referral;
-    }
-
-    public Boolean getRemote() {
-        return remote;
-    }
-
-    public void setRemote(Boolean remote) {
-        this.remote = remote;
-    }
-
-    public Boolean getGotResponse() {
-        return gotResponse;
-    }
-
-    public void setGotResponse(Boolean gotResponse) {
-        this.gotResponse = gotResponse;
-    }
-
-    public String getJobUrlLink() {
-        return jobUrlLink;
-    }
-
-    public void setJobUrlLink(String jobUrlLink) {
-        this.jobUrlLink = jobUrlLink;
-    }
-
-    
 }
