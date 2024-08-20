42bb399565780013d0895335dd80127a22cfdf1a
matthew123003 <matthew123003@gmail.com>
2024-08-20 12:34:45 -0400

    Added contructors to User model class

--- TekTrackBackEnd/Track/src/main/java/Models/User.java
+++ TekTrackBackEnd/Track/src/main/java/Models/User.java
@@ -45,6 +45,18 @@ public class User {
 
     public User() {} // Default constructor, required by JPA.
 
+    public User(String userName) { // Created only for basic Mockito test
+        this.userName = userName;
+    }
+
+    public User(String fName, String lName, String email, String userName, String password) { // Constructor created for testing with Mockito
+        this.fName = fName; // Sets the user's first name.
+        this.lName = lName; // Sets the user's last name.
+        this.email = email; // Sets the user's email address.
+        this.userName = userName; // Sets the user's username.
+        this.password = password; // Sets the user's password.
+    }
+
     public User(String fName, String lName, String email, String userName, String password, List<JobInfo> jobInfo) { // Constructor to initialize a User object without `userId`.
         this.fName = fName; // Sets the user's first name.
         this.lName = lName; // Sets the user's last name.
