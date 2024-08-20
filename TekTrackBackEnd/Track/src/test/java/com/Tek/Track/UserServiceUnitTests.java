d3f04852f6f40dc1659ddf3505b41f55196d221f
matthew123003 <matthew123003@gmail.com>
2024-08-20 11:06:39 -0400

    Started Unit tests, need to add testing dependencies

--- TekTrackBackEnd/Track/src/test/java/com/Tek/Track/UserServiceUnitTests.java
+++ TekTrackBackEnd/Track/src/test/java/com/Tek/Track/UserServiceUnitTests.java
@@ -1,5 +1,13 @@
 package com.Tek.Track;
 
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.junit.runner.RunWith;
+
+@ActiveProfiles("test")
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = TrackApplication.class)
 public class UserServiceUnitTests {
 
 }
