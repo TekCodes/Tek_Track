168ab7e7ad659be93ee3f56997b2fed30aa78a85
matthew123003 <matthew123003@gmail.com>
2024-08-20 13:56:11 -0400

    Started UserController tests

--- TekTrackBackEnd/Track/src/test/java/com/Tek/Track/UserControllerTests.java
+++ TekTrackBackEnd/Track/src/test/java/com/Tek/Track/UserControllerTests.java
@@ -1,5 +1,23 @@
 package com.Tek.Track;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import Controllers.UserController;
+
+@WebMvcTest(controllers = UserController.class)
+@ActiveProfiles("test")
 public class UserControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserController userController;
+
+    
 
 }
