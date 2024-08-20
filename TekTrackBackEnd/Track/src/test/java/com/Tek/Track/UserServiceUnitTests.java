2538ddc476199501ec7b368a875d9dd4b5bfa7ae
matthew123003 <matthew123003@gmail.com>
2024-08-20 11:14:17 -0400

    Autowired UserService to tests

--- TekTrackBackEnd/Track/src/test/java/com/Tek/Track/UserServiceUnitTests.java
+++ TekTrackBackEnd/Track/src/test/java/com/Tek/Track/UserServiceUnitTests.java
@@ -1,8 +1,12 @@
 package com.Tek.Track;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import Services.UserService;
+
 import org.junit.runner.RunWith;
 
 @ActiveProfiles("test")
@@ -10,4 +14,8 @@ import org.junit.runner.RunWith;
 @SpringBootTest(classes = TrackApplication.class)
 public class UserServiceUnitTests {
 
+    @Autowired
+    private UserService userService;
+
+    
 }
