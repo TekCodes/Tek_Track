197da855a7a666f0c5e66b15fb02e67f6e469b76
matthew123003 <matthew123003@gmail.com>
2024-08-21 18:05:16 -0400

    Added findAll test

--- TekTrackBackEnd/Track/src/test/java/com/Tek/Track/UserServiceUnitTests.java
+++ TekTrackBackEnd/Track/src/test/java/com/Tek/Track/UserServiceUnitTests.java
@@ -8,6 +8,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import Models.User;
 import Repositories.UserRepository;
 import Services.UserService;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import org.junit.Assert;
 import org.junit.Test;
@@ -120,6 +122,20 @@ public class UserServiceUnitTests {
         boolean isDeleted = userService.deleteByUserName(mockUserName);
 
         Assert.assertTrue(isDeleted);
+    }
+
+    @Test // Test findAll method in User Service
+    public void whenFindAllUsers_thenReturnUserList() {
+        List<User> mockUsers = new ArrayList<>();
+        mockUsers.add(new User("FirstName1", "LastName1", "email1@example.com", "user1", "password1"));
+        mockUsers.add(new User("FirstName2", "LastName2", "email2@example.com", "user2", "password2"));
+
+        Mockito.when(userRepository.findAll()).thenReturn(mockUsers);
+
+        List<User> result = userService.findAll();
+
+        Assert.assertNotNull(result);
+        Assert.assertEquals(2, result.size());
     }
 
     
