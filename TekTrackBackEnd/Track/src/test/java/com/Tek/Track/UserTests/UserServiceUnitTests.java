b7c9e297811de06fa46ef6059affcd78f4033509
matthew123003 <matthew123003@gmail.com>
2024-08-23 09:46:46 -0400

    Refactored tests for organization

--- TekTrackBackEnd/Track/src/test/java/com/Tek/Track/UserTests/UserServiceUnitTests.java
+++ TekTrackBackEnd/Track/src/test/java/com/Tek/Track/UserTests/UserServiceUnitTests.java
@@ -1,10 +1,11 @@
-package com.Tek.Track;
+package com.Tek.Track.UserTests;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import com.Tek.Track.TrackApplication;
 import Models.User;
 import Repositories.UserRepository;
 import Services.UserService;
