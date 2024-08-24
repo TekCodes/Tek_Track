8597c45a4c143cf0522cf7b0892d9cb6532a0fc4
matthew123003 <matthew123003@gmail.com>
2024-08-24 11:47:43 -0400

    Setup method for testing

--- TekTrackBackEnd/Track/src/test/java/com/Tek/Track/JobTests/JobModelUnitTests.java
+++ TekTrackBackEnd/Track/src/test/java/com/Tek/Track/JobTests/JobModelUnitTests.java
@@ -1,8 +1,12 @@
 package com.Tek.Track.JobTests;
 
+import java.util.Date;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import Models.JobInfo;
+import Models.User;
 
 public class JobModelUnitTests {
 
@@ -10,8 +14,17 @@ public class JobModelUnitTests {
     private JobInfo identicalJobInfo;
     private JobInfo differentJobInfo;
 
+    @Before
+    public void setUp() {
+        User user = new User();
+        Date dateApplied = new Date();
+
+        jobInfo = new JobInfo(1L, "Company", "Job Title", "http://joblink.com", "Job Description", dateApplied, "Contact Name", "contact@example.com", "1234567890", "Referral Name", true, true, user);
+
+        identicalJobInfo = new JobInfo(1L, "Company", "Job Title", "http://joblink.com", "Job Description", dateApplied, "Contact Name", "contact@example.com", "1234567890", "Referral Name", true, true, user);
+
+        differentJobInfo = new JobInfo(2L, "Different Company", "Different Job Title", "http://differentjoblink.com", "Different Job Description", new Date(), "Different Contact Name", "different@example.com", "0987654321", "Different Referral Name", false, false, user);
+    }
+
     
-
-
-
 }
