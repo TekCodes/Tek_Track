f778ab0f09255c3588b201a7163f7932f69c688b
matthew123003 <matthew123003@gmail.com>
2024-08-24 11:57:08 -0400

    Autowired and MockBean annotations added for services and repos

--- TekTrackBackEnd/Track/src/test/java/com/Tek/Track/JobTests/JobServiceUnitTests.java
+++ TekTrackBackEnd/Track/src/test/java/com/Tek/Track/JobTests/JobServiceUnitTests.java
@@ -1,16 +1,27 @@
 package com.Tek.Track.JobTests;
 
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.Tek.Track.TrackApplication;
 
+import Repositories.JobRepository;
+import Services.JobService;
+
 @ActiveProfiles("test")
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = TrackApplication.class)
 public class JobServiceUnitTests {
+
+    @MockBean
+    private JobRepository jobRepository;
+
+    @Autowired
+    private JobService jobService;
 
     
 
