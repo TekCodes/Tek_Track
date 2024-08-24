908577e6a499b1e997d06822ffbb4841761196a5
matthew123003 <matthew123003@gmail.com>
2024-08-24 11:58:40 -0400

    Started writing tests for JobService

--- TekTrackBackEnd/Track/src/test/java/com/Tek/Track/JobTests/JobServiceUnitTests.java
+++ TekTrackBackEnd/Track/src/test/java/com/Tek/Track/JobTests/JobServiceUnitTests.java
@@ -1,5 +1,6 @@
 package com.Tek.Track.JobTests;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,6 +24,9 @@ public class JobServiceUnitTests {
     @Autowired
     private JobService jobService;
 
-    
+    @Test
+    public void whenJobIdIsProvided_thenRetrievedJobIsCorrect() {
+        
+    }
 
 }
