1f44c31ed1b58a29d73b792479578706fa3d4fed
matthew123003 <matthew123003@gmail.com>
2024-08-24 11:55:20 -0400

    JobServiceUnit tests started

--- TekTrackBackEnd/Track/src/test/java/com/Tek/Track/JobTests/JobServiceUnitTests.java
+++ TekTrackBackEnd/Track/src/test/java/com/Tek/Track/JobTests/JobServiceUnitTests.java
@@ -1,5 +1,17 @@
 package com.Tek.Track.JobTests;
 
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.Tek.Track.TrackApplication;
+
+@ActiveProfiles("test")
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = TrackApplication.class)
 public class JobServiceUnitTests {
+
+    
 
 }
