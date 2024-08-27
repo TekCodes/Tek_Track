aeea0380b9bf9b9c92512f7382b2dabdba03d909
matthew123003 <matthew123003@gmail.com>
2024-08-27 16:40:57 -0400

    Started Job Controller tests

--- TekTrackBackEnd/Track/src/test/java/com/Tek/Track/JobTests/JobControllerTests.java
+++ TekTrackBackEnd/Track/src/test/java/com/Tek/Track/JobTests/JobControllerTests.java
@@ -1,5 +1,13 @@
 package com.Tek.Track.JobTests;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
+import com.Tek.Track.Controllers.JobController;
+
+@WebMvcTest(controllers = JobController.class)
+@ActiveProfiles("test")
 public class JobControllerTests {
+
+    
 
 }
