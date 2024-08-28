5ae63168f31b86b5caae4702024086cba83ec193
matthew123003 <matthew123003@gmail.com>
2024-08-28 11:10:11 -0400

    Setup for Job Controller tests

--- TekTrackBackEnd/Track/src/test/java/com/Tek/Track/JobTests/JobControllerTests.java
+++ TekTrackBackEnd/Track/src/test/java/com/Tek/Track/JobTests/JobControllerTests.java
@@ -1,13 +1,38 @@
 package com.Tek.Track.JobTests;
 
+import org.junit.Before;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.ActiveProfiles;
 import com.Tek.Track.Controllers.JobController;
+import com.Tek.Track.Models.JobInfo;
+import com.Tek.Track.Services.JobService;
 
 @WebMvcTest(controllers = JobController.class)
 @ActiveProfiles("test")
 public class JobControllerTests {
 
-    
+    @Mock
+    private JobService jobService;
+
+    @InjectMocks
+    private JobController jobController;
+
+    private JobInfo job1;
+    private JobInfo job2;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        job1 = new JobInfo();
+        job1.setJobInfoId(1L);
+        job1.setJobTitle("Software Engineer");
+
+        job2 = new JobInfo();
+        job2.setJobInfoId(2L);
+        job2.setJobTitle("Data Scientist");
+    }
 
 }
