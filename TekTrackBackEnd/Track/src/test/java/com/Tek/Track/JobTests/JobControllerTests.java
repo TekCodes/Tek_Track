21021fdb3c7b38e221baf27a4e1997beddc030e5
matthew123003 <matthew123003@gmail.com>
2024-08-28 11:21:42 -0400

    Job Controller test coverage 100%

--- TekTrackBackEnd/Track/src/test/java/com/Tek/Track/JobTests/JobControllerTests.java
+++ TekTrackBackEnd/Track/src/test/java/com/Tek/Track/JobTests/JobControllerTests.java
@@ -8,7 +8,6 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import java.util.ArrayList;
 import java.util.List;
-
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
