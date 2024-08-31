72f486c66213d9479f996322b8513d38c1fe616f
matthew123003 <matthew123003@gmail.com>
2024-08-31 12:37:53 -0400

    Updated Intership Repo

--- TekTrackBackEnd/Track/src/main/java/com/Tek/Track/Repositories/InternshipRepository.java
+++ TekTrackBackEnd/Track/src/main/java/com/Tek/Track/Repositories/InternshipRepository.java
@@ -1,10 +1,13 @@
 package com.Tek.Track.Repositories;
 
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import com.Tek.Track.Models.Internship;
 
 @Repository
 public interface InternshipRepository extends CrudRepository <Internship, Long>{
+
+    List<Internship> findByUserUserId(Long userId);
     
 }
