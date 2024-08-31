21c9b0fcb4e25ad0050904c65b55dc1f259204d0
matthew123003 <matthew123003@gmail.com>
2024-08-31 12:32:16 -0400

    Updated Job Repo for OAuth2 implementation

--- TekTrackBackEnd/Track/src/main/java/com/Tek/Track/Repositories/JobRepository.java
+++ TekTrackBackEnd/Track/src/main/java/com/Tek/Track/Repositories/JobRepository.java
@@ -1,5 +1,7 @@
 package com.Tek.Track.Repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.Tek.Track.Models.JobInfo;
 
 @Repository
 public interface JobRepository extends CrudRepository<JobInfo, Long> {
+
+    List<JobInfo> findByUserUserId(Long userId);
 
 }
