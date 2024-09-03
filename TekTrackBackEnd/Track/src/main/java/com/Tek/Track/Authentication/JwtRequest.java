396f0a648199d4acd718ba1d01bd1ffd0fc1b692
matthew123003 <matthew123003@gmail.com>
2024-09-02 21:39:12 -0400

    JwtAuthFilter, Request, and Response done

--- TekTrackBackEnd/Track/src/main/java/com/Tek/Track/Authentication/JwtRequest.java
+++ TekTrackBackEnd/Track/src/main/java/com/Tek/Track/Authentication/JwtRequest.java
@@ -1,5 +1,33 @@
 package com.Tek.Track.Authentication;
 
 public class JwtRequest {
+    private String username;
+    private String password;
 
+    // Default constructor
+    public JwtRequest() {}
+
+    // Parameterized constructor
+    public JwtRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    // Getter and Setter for username
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    // Getter and Setter for password
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
